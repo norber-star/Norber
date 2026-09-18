8b7321667b537a7c7ff0a1a6c51327b2b6a8e712
norber-star <norberrodaser@gmail.com>
2026-09-18 19:29:09 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://ycjznhqvexxbvicjvijp.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_h5FToBovkn4xM5RE7By3Rg_uTDI5kid';
+
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
